229a80f82ca7387441e10102912f25f39f23f36b
Samuel Fox <samueljasonfox@gmail.com>
2021-04-06 10:48:23 -0700

    initializing express server, linking mongo database, setting up schemas

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://i.imgur.com/owu9ukE.jpeg",
+    },
+    projects: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Session",
+        },
+    ],
+    date: { type: Date, default: Date.now },
+});
+
+UserSchema.pre("save", function (next) { // 
+    const user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) return next();
+
+    // generate a salt
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,35 @@
+// Set up the Express app
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+// const cors = require("cors");
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", '*');
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//   next();
+// });
+// app.use(cors());
+
+// Database
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trakmyproject", { // connect to database
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+// User Routes
+const userRoutes = require("./controllers/user");
+app.use(userRoutes);
+
+// Server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
