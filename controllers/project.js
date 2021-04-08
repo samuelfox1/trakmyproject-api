d98450b903fac3553ab196d5c0b12837a586c87b
freeStoke <samueljasonfox@gmail.com>
2021-04-07 20:53:36 -0700

    changed property 'name' to 'title' in models/project.js. Completed delete routes for controllers/project/js

--- controllers/project.js
+++ controllers/project.js
@@ -15,9 +15,8 @@ router.post('/api/project', (req, res) => {
 
 router.put('/api/project', (req, res) => {
     let x = req.body
-    console.log(x)
     db.Project.findByIdAndUpdate(
-        req.body.id, // id to find
+        x.id, // id to find
         { // data to update
             name: x.name,
             public: x.public,
@@ -30,6 +29,56 @@ router.put('/api/project', (req, res) => {
         .then(data => res.json(data))
         .catch(err => res.status(500).json(err)
         )
+})
+
+router.delete('/api/project', async (req, res) => {
+    const rb = req.body
+    const projectToDelete = await findProjectToDelete(rb)
+    if (!projectToDelete) res.status(500).json('invalid id to delete')
+    if (projectToDelete.admin != rb.user_id) res.status(500).json('not authorized to delete')
+    else {
+        const updatedProjectsArr = await getUpdatedProjectsArr(rb)
+        const updated = await updateAdminsProjects(rb, updatedProjectsArr)
+        const deleted = await deleteProject(rb)
+        if (updated && deleted) res.json({ message: 'delete success', updatedProjects: updated.projects })
+        else res.status(500).json('delete failure')
+    }
+
+    function findProjectToDelete(rb) {
+        return new Promise((resolve, reject) => {
+            db.Project.findById(rb.project_id)
+                .then(p => resolve(p))
+                .catch(err => reject({ admin: null }))
+        })
+    }
+
+    function deleteProject(rb) {
+        return new Promise((resolve, reject) => {
+            db.Project.findByIdAndDelete(rb.project_id)
+                .then(data => resolve(data))
+                .catch(err => reject(null))
+        })
+    }
+
+    function getUpdatedProjectsArr(rb) {
+        return new Promise((resolve, reject) => {
+            db.User.findById(rb.user_id)
+                .then(admin => {
+                    const newArr = admin.projects.filter(p => p._id != rb.project_id)
+                    resolve(newArr)
+                })
+                .catch(err => reject(null))
+        })
+    }
+
+    function updateAdminsProjects(rb, newArray) {
+        return new Promise((resolve, reject) => {
+            if (!newArray) reject(false)
+            db.User.findByIdAndUpdate(rb.user_id, { $set: { projects: newArray } }, { new: true }).populate("projects") // remove the project id from admin's projects property
+                .then(data => resolve(data))
+                .catch(err => reject(false))
+        })
+    }
 })
 
 module.exports = router
