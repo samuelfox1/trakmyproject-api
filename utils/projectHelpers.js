574f1e0ec0edb602b99e5620e185bb89bd455a60
Samuel Fox <samueljasonfox@gmail.com>
2021-08-24 09:21:22 -0700

    add: promise rejections instead of throwing errors

--- utils/projectHelpers.js
+++ utils/projectHelpers.js
@@ -1,57 +1,55 @@
 const db = require("../models");
 
-const handleError = (err, message) => { throw new Error({ error: err, message: message }) }
-
 const createProject = (body) => {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         try {
-
             db.Project.create(body)
                 .then(data => resolve(data))
-                .catch(err => handleError(err, 'failed to create project'))
-
-        } catch (err) { handleError(err, 'createProject() failed') }
+                .catch(error => reject(error))
+        } catch (error) {
+            reject(error)
+        }
     })
 }
 
 const findProjectById = (id) => {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         try {
-
             db.Project.findById(id)
                 .then(data => resolve(data))
-                .catch(err => handleError(err, 'failed to find project my id'))
-
-        } catch (err) { handleError(err, 'findProjectById() failed') }
+                .catch(error => reject(error))
+        } catch (error) {
+            reject(error)
+        }
     })
 }
 
 const updateProject = (id, data) => {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         try {
-
             db.Project.findByIdAndUpdate(id, { data: data }, { new: true }).populate("posts")
                 .then(data => resolve(data))
-                .catch(err => handleError(err, 'project to update not found'))
-
-        } catch (err) { handleError(err, 'updateProject() failed') }
+                .catch(error => reject(error))
+        } catch (error) {
+            reject(error)
+        }
     })
 }
 
 const deleteProject = (id) => {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         try {
-
             db.Project.findByIdAndDelete(id)
                 .then(data => resolve(data))
-                .catch(err => handleError(err, 'project to delete not found'))
-
-        } catch (err) { handleError(err, 'deleteProject() failed') }
+                .catch(error => reject(error))
+        } catch (error) {
+            reject(error)
+        }
     })
 }
 
 // const addPostToProject = (rb, postId) => {
-//     return new Promise((resolve, reject) => {
+//     return new Promise(((resolve, reject) => {
 //         db.Project.findByIdAndUpdate(
 //             rb.project_id,
 //             { $addToSet: { posts: postId } },
@@ -59,7 +57,7 @@ const deleteProject = (id) => {
 //         )
 //             .populate("posts")
 //             .then(data => resolve(data))
-//             .catch(err => reject(err))
+//             .catch(error => reject(error))
 //     })
 // }
 
