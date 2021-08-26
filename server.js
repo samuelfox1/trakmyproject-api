e65dc7269919b2c144711befb39807e6336d7478
Samuel Fox <samueljasonfox@gmail.com>
2021-08-26 02:02:13 -0700

    add: develop script to package.json and disabling certificate verifaction for testing

--- server.js
+++ server.js
@@ -2,20 +2,27 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require('dotenv').config()
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+/**
+ * Logs an error
+ * (node:20713) Warning: Setting the NODE_TLS_REJECT_UNAUTHORIZED environment variable to '0' makes TLS connections and HTTPS requests insecure by disabling certificate verification.
+(Use `node --trace-warnings ...` to show where the warning was created)
+ */
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 app.use(cors());
 
 const mongoose = require("mongoose");
