a35ca3d97155ebde462dfe9a6bf3f84b77856520
Samuel Fox <samueljasonfox@gmail.com>
2021-08-26 03:53:35 -0700

    revert: commenting out cors

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", '*');
-//     res.header("Access-Control-Allow-Credentials", true);
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use(cors());
 
 const mongoose = require("mongoose");
